d9f9baa4fa31d58a7d857a625fa5592e30850931
sezaite <martyna.sezaite@gmail.com>
2021-03-17 21:06:34 +0000

    base

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php
-$dist = 0;
+session_start();
+define('API', 'https://api.exchangeratesapi.io/latest');
+$fromSum;
+$fromCur;
+$toSum;
+$toCur;
+if(!empty($_POST)){
+    $curl= curl_init();
+    curl_setopt($curl, CURLOPT_URL, API);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    $answer = curl_exec($curl);
+    curl_close($curl);
+    $answer = json_decode($answer);
+
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -8,9 +23,30 @@ $dist = 0;
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="style.css?<?=filemtime("style.css")?>" rel="stylesheet" type="text/css" />
+    <title>Currency converter</title>
 </head>
 <body>
-    <h2>Atstumas <?= $dist ?></h2>
+<main class='converter'>
+    <h1>Convert NOW only for 9.99!!!</h1>
+    <form action='' method='post'>
+    <input type='number'></input>
+    <select name="" id="">
+    <option value="Curr"></option>
+    </select>
+    <input type='number'></input>
+    <select name="" id="">
+    <option value="Curr"></option>
+    </select>
+    <button type='submit'>Convert</button>
+</form>
+    <?php 
+    if (isset($_SESSION['fromSum']) && isset($_SESSION['toSum'])){
+        echo "<h2><?=$fromSum . $fromCur . 'converted into' . $toCur . 'equals' . $toSum ?></h2>";
+        session_destroy();
+    }
+    
+    ?>
+</main>
 </body>
 </html>
